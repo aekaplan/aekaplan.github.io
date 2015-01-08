73011e7565b070f726e3227e753bb31fb95b6f48
Adam Kaplan <akaplan@8thlight.com>
2015-01-07 22:19:57 -0600

    removes site folder

--- _site/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function($) {
-
-  $(".scroll").click(function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 400);
-  });
-
-});
